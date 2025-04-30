85716dc80c3307b6b7d71f04d3961c0cdb59bd60
A5TR0spud <57902759+A5TR0spud@users.noreply.github.com>
2025-04-29 21:40:16 -0400

    mayday full sprites archival

--- obj/Debug/net8.0/SuperfluityTwo.AssemblyInfo.cs
+++ obj/Debug/net8.0/SuperfluityTwo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SuperfluityTwo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9980e3e3d1f78139882d6872a7b1756e488c98c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0433ef948fd5f5c741681710a62acc0798841a33")]
 [assembly: System.Reflection.AssemblyProductAttribute("SuperfluityTwo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SuperfluityTwo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
